8248265c9b44b4ff2ac7fd209211879fbd935dc1
Adrian3312401110 <adrianrizqullah4@gmail.com>
2025-10-16 20:25:07 +0700

    Membuat halaman pembuka dan dashboard (frontend)

---
+++ resources/views/sections/contact.blade.php
@@ -0,0 +1,26 @@
+<section id="contact">
+  <div class="contact-wrapper">
+    <div class="contact-form">
+      <h2>Contact Us</h2>
+      <p class="subtext">Feel free to contact us at anytime. We will get back as soon as possible.</p>
+      <form>
+        <input type="text" placeholder="Name">
+        <input type="email" placeholder="Email">
+        <textarea placeholder="Message"></textarea>
+        <button type="submit">Send</button>
+      </form>
+    </div>
+    <div class="contact-info">
+      <h3>Info</h3>
+      <p>📧 Info@gmail.com</p>
+      <p>📞 +9955442318</p>
+      <p>👤 John</p>
+      <p>🕒 08:56</p>
+      <div class="social-icons">
+        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" alt="Facebook"></a>
+        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733579.png" alt="Twitter"></a>
+        <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/300/300221.png" alt="Google"></a>
+      </div>
+    </div>
+  </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 
-Route::get('/', [LandingController::class, 'index'])->name('landing');
+Route::get('/', function () {
+    return view('landing');
+});
+
