613cd281b5319e1f5d8903289b5ab4be8fc1df9b
Adrian3312401110 <adrianrizqullah4@gmail.com>
2025-12-17 11:50:32 +0700

    Update Procfile and fix HTTPS configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+        URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS', 'on');
+        }
         // Hanya force HTTPS di production
         // if ($this->app->environment('production')) {
         //     URL::forceScheme('https');
