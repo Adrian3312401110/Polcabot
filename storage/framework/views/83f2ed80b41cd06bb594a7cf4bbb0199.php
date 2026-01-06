6fe6c586ed429e6b1804a217347817db3a63a748
Helena2006854 <helenagultom2006@gmail.com>
2026-01-07 01:10:30 +0700

    penambahan dataset

---
+++ storage/framework/views/83f2ed80b41cd06bb594a7cf4bbb0199.php
@@ -0,0 +1,9 @@
+<section id="about">
+    <img src="https://i.pinimg.com/736x/b9/b6/0e/b9b60eb215193ce939ca6721601a846b.jpg" alt="team">
+    <div class="text">
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <button onclick="location.href='#contact'">Contact Us</button>
+    </div>
+</section>
+<?php /**PATH E:\Polcabot\resources\views/components/about.blade.php ENDPATH**/ ?>
